130d080d2acd3655099cb3c6f81d22483ee6351f
Rob April <135466939+RobApril@users.noreply.github.com>
2024-04-24 15:18:52 +0200

    API Url

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -24,7 +24,7 @@ $(function () {
             var json = {name: name, phone: phone, email: email, message: message}
             $.ajax({
                 // --- CHANGE THIS LINE TO YOUR OWN API GATEWAY  -------- 
-                url: "{Insert your own AWS API Gateway Endpoint URL here}",
+                url: "https://yougjo7n2g.execute-api.eu-west-1.amazonaws.com/2401PTDE-ROBAPR-CC-predict-lambda",
                 // ------------------------------------------------------  
                 type: "POST",
                 data: JSON.stringify(json),
